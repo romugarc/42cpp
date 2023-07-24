d7416557cc911c3335b1fcb32b197ac4e0d4017c
romugarc <90500337+romugarc@users.noreply.github.com>
2023-07-24 08:15:45 +0200

    Update main.cpp

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -7,34 +7,18 @@
 
 int main()
 {
-	/*const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-
-	delete i;
-	delete j;
-	delete meta;
-
-	const WrongAnimal* wrong = new WrongAnimal();
-	const WrongAnimal* k = new WrongCat();
-
-	std::cout << k->getType() << " " << std::endl;
-	k->makeSound(); //will output the wrongcat sound!
-	wrong->makeSound();
-
-	delete k;
-	delete wrong;*/
-
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	delete j;//should not create a leak
 	delete i;
+
+	const Animal* doga = new Dog();
+	const Animal* dogb(doga);
+	const Animal* dogc = dogb;
+
+	const Animal* cata = new Cat();
+	const Animal* catb(cata);
+	const Animal* catc = catb;
 
 	return 0;
 }
