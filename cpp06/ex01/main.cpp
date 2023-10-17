7948bf53c563f3fabf8f9d0255f52e1a2991820c
Your Name <warnora@MSI>
2023-10-17 04:23:33 +0200

    Serializer

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -1,15 +1,17 @@
-#include "Serialize.hpp"
+#include "Serializer.hpp"
 #include "Data.hpp"
 
 int main (void)
 {
     Data *dat = new Data;
-    Serialize seri;
+    Serializer seri;
     uintptr_t num;
 
     std::cout << dat->getReal() << std::endl;
     std::cout << dat->getNum() << std::endl;
     num = seri.serialize(dat);
+    std::cout << dat->getReal() << std::endl;
+    std::cout << dat->getNum() << std::endl;
     dat = seri.deserialize(num);
     std::cout << dat->getReal() << std::endl;
     std::cout << dat->getNum() << std::endl;
