bc794bc72af38a409be1733f939edfd67c022d84
Romuald Garcia <rgarcia@z2r12p4.42lyon.fr>
2023-10-25 18:31:12 +0200

    tests ex01 done

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -27,8 +27,11 @@ int	main( void )
 	int	cho[7] = {7, 7, 7, 7, 7, 7, 7};
 
 	::iter(cho, 7, plus);
+	std::cout << "chaussettes dans le tiroir a chaussettes : " << std::endl;
 	::iter(chaussettes, 4, display);
+	std::cout << "\nnombre de paires de chaussettes: " << std::endl;
 	for (int i = 0; i < 7; i++)
-		std::cout << cho[i] << std::endl;
+		std::cout << cho[i] << " ";
+	std::cout << std::endl;
 	return 0;
 }
